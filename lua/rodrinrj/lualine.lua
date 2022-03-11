7a5520ee9d03f2886e4f4fb03fda5aa805e11c47
Rodrigo Naranjo <rodrigo.naranjo@getjusto.com>
2022-03-11 00:06:05 -0300

    Full neovim config as of 10/03/2022

---
+++ lua/rodrinrj/lualine.lua
@@ -0,0 +1,78 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+	return
+end
+
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = true,
+	update_in_insert = false,
+	always_visible = true,
+}
+
+local diff = {
+	"diff",
+	colored = false,
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  cond = hide_in_width
+}
+
+local branch = {
+  "branch",
+  icons_enabled = true,
+  icon = "",
+}
+
+local location = {
+  "location",
+ padding = 0,
+}
+
+-- cool function for progress
+local progress = function()
+	local current_line = vim.fn.line(".")
+	local total_lines = vim.fn.line("$")
+	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	local line_ratio = current_line / total_lines
+	local index = math.ceil(line_ratio * #chars)
+	return chars[index]
+end
+
+lualine.setup {
+  options = {
+    theme = 'onedark',
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = { "NvimTree" }
+  },
+  sections = {
+    lualine_a = {
+      { 'mode', separator = { left = '' }, right_padding = 2 },
+    },
+    lualine_b = { 'filename', branch },
+    lualine_c = { location },
+    lualine_x = { diagnostics },
+    lualine_y = { 'filetype', progress },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
+}
+
